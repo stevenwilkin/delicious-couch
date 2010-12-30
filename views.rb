b43d05b9ecd3d3e0f3c3e451fe201a7197e86f75
Steven Wilkin <stevenwilkin@gmail.com>
2010-12-30 01:40:10 +0000

    add first cut of script responsible for creating the design doc
    contains view to return tags and their counts

---
+++ views.rb
@@ -0,0 +1,31 @@
+#!/usr/bin/env ruby
+
+require 'rubygems'
+require 'couchrest'
+
+server = CouchRest.new
+db = server.database('delicious')
+
+# delete design doc if it already exists
+db.delete_doc db.get('_design/links') rescue nil
+
+# save design doc containing views
+db.save_doc({
+  '_id' => '_design/links',
+  :views => {
+    :tags => {
+      :map => '
+        function(doc) {
+        	doc.tag.forEach(function(tag) {
+        		emit(tag, 1);
+        	});
+        }
+      ',
+      :reduce => '
+        function(keys, values) {
+        	return sum(values);	
+        }
+      '
+    }
+  }
+})
